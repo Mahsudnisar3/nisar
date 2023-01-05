ab6a11bd7278eaeeeeff61e4ac0be4b599032ca1
Mahsudnisar3 <63830023+Mahsudnisar3@users.noreply.github.com>
2023-01-05 21:49:55 +0500

    Update nisar.py

--- nisar.py
+++ nisar.py
@@ -1,1 +1,9 @@
-h
+import platform
+
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+          __import__("n").nisar()
+elif 'aarch' in arc:
+              __import__("n").nisar()
+else:
+        exit(f' Unknow device machine {arc}')
