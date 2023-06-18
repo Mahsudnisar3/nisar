08b5b4f0daa64bfebca33431a66c05ae13c371fa
Mahsudnisar3 <63830023+Mahsudnisar3@users.noreply.github.com>
2023-06-18 09:56:05 +0500

    Update nisar.py

--- nisar.py
+++ nisar.py
@@ -1,8 +1,16 @@
-import platform
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-          __import__("t").main()
-elif 'aarch' in arc:
-              __import__("t").main()
-else:
-        exit(f' Unknow device machine {arc}')
+import os,platform
+os.system('git pull')
+
+nis=platform.architecture()[0]
+if nis=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif trt=="64bit":
+    __import__("t")
+
+
+
+
+
+
+
+
